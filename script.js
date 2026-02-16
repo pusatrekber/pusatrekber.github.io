9cf6981a7b5824606867e5b8f834bdbe7797eb96
pusatrekber <Anggitadeewi09@gmail.com>
2026-02-16 13:26:11 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,40 @@
+// ============================
+// PROFIL UTAMA
+// ============================
+document.getElementById("avatar").src = "PUSAT_REKBER_PK.png";
+
+document.getElementById("title").textContent = "PUSAT REKBER";
+document.getElementById("subtitle").textContent =
+  "Rekber Aman • Cepat • Terpercaya";
+
+
+// ============================
+// LIST TOMBOL LINK
+// ============================
+document.getElementById("links").innerHTML = `
+
+  <a class="btn" href="https://wa.me/6282364681126" target="_blank">
+    💬 WHATSAPP ADMIN
+  </a>
+
+  <a class="btn" href="https://wa.me/60XXXXXXXXX" target="_blank">
+    🇲🇾 WHATSAPP ADMIN MALAY
+  </a>
+
+  <a class="btn" href="https://instagram.com/USERNAME_LO" target="_blank">
+    📷 INSTAGRAM RESMI
+  </a>
+
+  <a class="btn" href="https://facebook.com/" target="_blank">
+    📘 FACEBOOK ADMIN
+  </a>
+
+  <a class="btn" href="#" target="_blank">
+    ✅ TESTIMONI TRUSTED
+  </a>
+
+  <a class="btn" href="#" target="_blank">
+    🛒 BIAYA REKBER
+  </a>
+
+`;
