0521330c2044d58e3ee477055b7b56c49e99560a
pusatrekber <Anggitadeewi09@gmail.com>
2026-02-16 15:49:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -24,16 +24,17 @@ document.getElementById("links").innerHTML = `
 
   <a class="btn" href="https://instagram.com/kemaluantidaklagiada" target="_blank">
   <img class="icon" src="https://cdn-icons-png.flaticon.com/512/733/733558.png">
-  INSTAGRAM RESMI PUSAT REKBER
+  INSTAGRAM PUSAT REKBER
 </a>
 
   <a class="btn" href="https://facebook.com/https://www.facebook.com/bikram.bhandari.716195" target="_blank">
   <img class="icon" src="https://cdn-icons-png.flaticon.com/512/733/733547.png">
-   FACEBOOK RESMI PUSAT REKBER
+   FACEBOOK PUSAT REKBER
   </a>
 
   <a class="btn" href="https://www.instagram.com/stories/highlights/17967262886866202/" target="_blank">
-    ✅ TESTIMONI PUSAT REKBER
+  <img class="icon" src="https://cdn-icons-png.flaticon.com/512/1828/1828640.png">
+   TESTIMONI PUSAT REKBER
   </a>
 
   <a class="btn" href="https://www.instagram.com/stories/highlights/17924194713229942/" target="_blank">
